52654f18cc6411916ebecead8359fb4bf7ab6a34
arupdas7887 <arupdas7887@gmail.com>
2025-07-20 18:54:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,15 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-BOT_TOKEN = "7673817380: AAH8NKM1A3kJzB9HVdWB1rkTIaMBeol6Nyk"  # Replace this with your actual token
+BOT_TOKEN = "7673817380: AAH8NKM1A3kJzB9HVdWB1rkTIaMBeol6Nyk""
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello! I'm alive on Render!")
 
 def main():
-    app = Application.builder().token(BOT_TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    app.run_polling()
+    application = Application.builder().token(BOT_TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    application.run_polling()
 
 if __name__ == "__main__":
     main()
